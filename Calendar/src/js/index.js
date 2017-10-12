dafaa634b55c2229b62d161f90f558fef5c87730
stone <stone-lyl@outlook.com>
2017-10-12 23:07:20 +0800

    标记今天和回到今天的功能完成~~~///(^v^)\\\~~~

--- Calendar/src/js/index.js
+++ Calendar/src/js/index.js
@@ -12,14 +12,34 @@ $(function () {
         $("#year").append(option);
     }
 
+    // 标记今天
+    let date = new Date();
+
+
     // 判断改变选择的对象是年还是月
-    $(document).on("change", "#month, #year", function (event) {
+    $("article").on("change", "#month, #year", function (event) {
         const value = this.value;
         if (value < 13)
             selectMonth = value;
         else selectYear = value;
         changeCalendar(selectYear, selectMonth)
+    });
+
+    //点击回到今天
+    $("article").on("click", "#resetToday", function () {
+        // $("#month").val(date.getMonth()+1);
+        // $("#year").val(date.getFullYear());
+        let date = new Date();
+
+        backToday(date);
     })
+    // 回到今天
+    function backToday(date) {
+        console.log(date, "date");
+        $("#month").val(date.getMonth() + 1);
+        $("#year").val(date.getFullYear());
+        changeCalendar(date.getFullYear(), date.getMonth() + 1);
+    }
 
     // 生成对应的日历
     function changeCalendar(selectYear, selectMonth) {
@@ -27,14 +47,15 @@ $(function () {
         const dayInMon = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
         let day, month, year, monthLength, date = new Date();
 
+
         day = date.getDate();
         month = (isNaN(selectMonth) || selectMonth === null) ? date.getMonth() : (selectMonth - 1);
         year = (isNaN(selectYear) || selectYear === null) ? date.getFullYear() : selectYear;
-        date = new Date(year, month, day);
+        // date = new Date(year, month, day+1);
 
         let firstDay = new Date(year, month, 1);        //获取当月的第一天
         let weekDay = firstDay.getDay();
-        console.log(date, day, 'month', month, 'year', year, firstDay, weekDay);
+        console.log(day, 'month', month, 'year', year, firstDay, weekDay);
 
         // 判断是否为闰年，计算二月的天数
         let judgeLength = (month) => {
@@ -47,7 +68,17 @@ $(function () {
                 return monthLength;
             }
         }
+        //生成date对象用于记录
+        function dateObject(day, month, year) {
+            this.day = day;
+            this.month = month;
+            this.year = year;
+        }
 
+        //点击获得当天的年月日 someday();
+
+
+        // 获取一个月日历中开始的日期
         let getStartDay = (preMonth, weekDay) => {
             let preMonthLength = judgeLength(preMonth);
             console.log(preMonthLength);
@@ -56,8 +87,9 @@ $(function () {
             let startDay = preMonthLength - weekDay + 2;
             // 用于校验日历当前的开始时间是否为周一
             let startDayDate = new Date(year, month - 1, startDay);
+            let startDate = new dateObject(startDay, month - 1, year);
             if (startDayDate.getDay() === 1) {
-                return startDay;
+                return startDate;
             } else {
                 return error;
             }
@@ -65,24 +97,30 @@ $(function () {
 
         let tr = document.querySelectorAll('tr');
         // 当前日历开始的日期
-        let cellDay = getStartDay(month - 1, weekDay);
-        console.log(tr, cellDay);
-        let creatMonHtml = (cellDay, month) => {
-            let currentTr, td, judgeMon = false;
+        let cellDate = getStartDay(month - 1, weekDay);
+        console.log(tr, cellDate);
+        let creatMonHtml = (cellDate, month) => {
+            let currentTr, td, cellDay = cellDate.day;
+            let cellMon = cellDate.month;
+            console.log("month", month, cellMon);
             for (let i = 1; i <= 6; i++) {
                 currentTr = tr[i];
                 currentTr.innerHTML = "";
 
                 for (let j = 0; j <= 6; j++) {
                     td = $("<td></td>").text(cellDay);
+
+                    //标记今天
+                    if ((cellDay === day) && (cellMon === month)) {
+                        td.css("background-color", "white");
+                    }
                     $(currentTr).append(td);
                     cellDay++;
-
                     // 判断是否为上个月的结尾
-                    if ((cellDay > judgeLength(month - 1)) && (!judgeMon)) {
+                    if ((cellDay > judgeLength(month - 1)) && (cellMon === month - 1)) {
                         cellDay = 1;
-                        judgeMon = true;
-                    } else if ((cellDay > judgeLength(month)) && judgeMon) {
+                        cellMon = month;
+                    } else if ((cellDay > judgeLength(month)) && (cellMon === month)) {
                         cellDay = 1;
                     }
 
@@ -90,7 +128,7 @@ $(function () {
             }
 
         }
-        creatMonHtml(cellDay, month);
+        creatMonHtml(cellDate, month);
     }
 
     changeCalendar(selectYear, selectMonth);
