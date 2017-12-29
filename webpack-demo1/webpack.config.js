9cea18b315fe178925cba5e16fac08bed36ae944
stone-lyl <stone-lyl@outlook.com>
2017-12-29 15:39:51 +0800

    基本完善

--- webpack-demo1/webpack.config.js
+++ webpack-demo1/webpack.config.js
@@ -3,12 +3,19 @@
 const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     entry: path.resolve(__dirname, 'app/main.js'), // 唯一入口文件
     output: {
         path: path.resolve(__dirname, 'build'), // 打包后文件存放的位置
-        filename: 'js/[name]-[chunkhash].js', // 打包后输出文件的文件名
+        filename: 'js/[name].js', // 打包后输出文件的文件名
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'build'),  // 服务器所加载的页面目录
+        inline: true,
+        port: 2333,
+        hot: true,
     },
     module: {
         loaders: [
@@ -48,17 +55,30 @@ module.exports = {
                             browser: ['last 5 versions']        // 浏览器最新的五个版本。
                         }
                     },
-                    {   loader: 'less-loader'   },
+                    { loader: 'less-loader' },
                 ]
             },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                include: '/app',
+                exclude: /node_modules/,
+                query: { presets: ['latest'] }
+            }
         ]
     },
     plugins: [
+        // new CleanWebpackPlugin('build/js/*.*', {
+        //     root: __dirname,
+        //     verbose: true,
+        //     dry: false,
+        // }),
         new HtmlWebpackPlugin({
             template: path.join(__dirname, 'index.html'),      // 被编译的HTML文件路径 
             filename: path.resolve(__dirname, 'build/index.html'),  // 编译后的HTML文件存放路径
             inject: 'body',      // 编译后的js被插入HTML的body中。
-            title: 'webpack is great!' //可以通过模板引入HTML文件中。
+            title: 'webpack is great!', //可以通过模板引入HTML文件中。
         }),
+        new webpack.HotModuleReplacementPlugin(),
     ],
 };
