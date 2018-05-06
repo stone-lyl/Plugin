40ec2177515805f4705cbc3a7d52ba4168e51283
stone <stone-lyl@outlook.com>
2018-05-06 11:08:22 +0800

    移除不必要文件

--- fireAlarm/app/page/notfound.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const NotFound = () => {
-    return (<div>
-        <h1> sorry, can't find that.</h1>
-    </div>)
-}
-export default NotFound;
--- webapck-route/app/page/home.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const Home = () => (
-    <div>
-        <h1> this is a Home component! </h1>
-    </div>
-)
-export default Home;
